3654a81cd54956ecfc1c76650b1916ef26d74919
PAK1 <piotr.adamczyk@3ds.com>
2022-08-31 16:26:27 +0200

    Issue 39: Change implementation of Db configuration class to separate class for each entity

---
+++ ClimbingAPI/Configuration/RoleConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using ClimbingAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ClimbingAPI.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.Property(x => x.Name)
+                .IsRequired();
+        }
+    }
+}
