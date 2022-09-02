f5d397f9030680608fee4448468a12f1d32b59ef
PAK1 <piotr.adamczyk@3ds.com>
2022-09-02 12:06:30 +0200

    Issue 38: find possibility to add index and implement it

--- ClimbingAPI/Migrations/20220902100323_indexes.Designer.cs
+++ ClimbingAPI/Migrations/20220902100323_indexes.Designer.cs
@@ -4,14 +4,16 @@ using ClimbingAPI.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ClimbingAPI.Migrations
 {
     [DbContext(typeof(ClimbingDbContext))]
-    partial class ClimbingDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220902100323_indexes")]
+    partial class indexes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
